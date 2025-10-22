6dc890f1d00b6e8c532bab6dd518e5f8b622807c
Juan Troconis <jatroconis4@gmail.com>
2025-10-21 21:52:55 -0500

    config styles and env

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  tmdbBaseUrl: 'https://api.themoviedb.org/3',
+  tmdbImageBase: 'https://image.tmdb.org/t/p',
+  openWeatherBaseUrl: 'https://api.openweathermap.org',
+  openWeatherVersion: '3.0',
+  tmdbApiKey: '',
+  openWeatherApiKey: '',
+};
